65e4d2daad2329037d6a3b536cf6ca3ce96835df
premnarayanp <premnarayanpatel223@gmail.com>
2024-02-16 02:08:33 +0530

    Bookmark and likedPost  Tab added in Profile section and NewsCard rendereed with FlatList in tab

--- tabScreen/BookMark.js
+++ tabScreen/BookMark.js
@@ -1,10 +1,15 @@
-import { Button, Text, View } from 'react-native';
+import { FlatList, View } from 'native-base';
+import { NewsCard } from '../components/index';
+import { articles } from '../constants/index';
 export default function BookMark({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>BookMark Tab</Text>
-
-            <Button title="Go back" onPress={() => navigation.goBack()} />
+        <View p='1'>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => <NewsCard article={item} CRUD_IconName="close" />}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     );
 }
--- tabScreen/LikedPost.js
+++ tabScreen/LikedPost.js
@@ -1,10 +1,15 @@
-import { Button, Text, View } from 'react-native';
+import { FlatList, View } from 'native-base';
+import { NewsCard } from '../components/index';
+import { articles } from '../constants/index';
 export default function LikedPost({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>LikedPost Tab</Text>
-
-            <Button title="Go back" onPress={() => navigation.goBack()} />
+        <View p='1'>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => <NewsCard article={item} CRUD_IconName="heart" CRUD_IconColor="#e91e63" />}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     );
 }
