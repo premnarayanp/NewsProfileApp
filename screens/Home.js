59e24bd45bf156a783544cf1dcd3af1f519f6aff
premnarayanp <premnarayanpatel223@gmail.com>
2024-02-16 02:31:00 +0530

    News In Home page using NewsCard

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,17 @@
-import { Button, Text, View } from 'react-native';
+import { FlatList, View, NativeBaseProvider } from 'native-base';
+import { NewsCard } from '../components/index';
+import { articles } from '../constants/index';
 export default function Home({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
-
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-
-    </View>
+    <NativeBaseProvider>
+      <View p='1'>
+        <FlatList
+          data={articles}
+          renderItem={({ item }) => <NewsCard article={item} isShowCRUD_Icon={false} />}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </NativeBaseProvider>
   );
 }
