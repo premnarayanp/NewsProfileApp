1ac0af63faa6839fc99c614141ccb7ae2bfa98ec
premnarayanp <premnarayanpatel223@gmail.com>
2024-02-16 02:21:42 +0530

    Profile Done

--- screens/Profile.js
+++ screens/Profile.js
@@ -10,14 +10,12 @@ export default function Profile({ navigation }) {
 
     return (
         <NativeBaseProvider>
-            <HStack space={3} justifyContent="center" alignItems='center'>
+            <HStack space={3} justifyContent="center" alignItems='center' mb='2'>
 
                 <Center>
-                    <Avatar bg="pink.600" alignSelf="center" size="xl" source={{
+                    <Avatar bg="pink.600" alignSelf="center" size="md" source={{
                         uri: "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2876&q=80"
-                    }}>
-                        GG
-                    </Avatar>
+                    }}> </Avatar>
                     <Text fontSize="md" bold>Premnarayan Patel</Text>
                 </Center>
 
@@ -37,7 +35,7 @@ export default function Profile({ navigation }) {
                 </Center>
             </HStack>
 
-            <HStack justifyContent="space-between" alignItems='center'>
+            <HStack justifyContent="space-between" alignItems='center' mb='2'>
                 <Button style={styles.btnColorLightPink} px="8" py='0.5'>Edit Profile</Button>
                 <Button style={styles.btnColorLightPink} px="8" py='0.5'>Share Profile</Button>
                 <IconButton style={styles.btnColorLightPink} padding="0.5" icon={<Icon color="black" size="lg" as={MaterialCommunityIcons} name="account-plus" />} />
